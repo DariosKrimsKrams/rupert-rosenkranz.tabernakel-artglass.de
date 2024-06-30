b210672fecac823307e97ec83eaa2db9c718a226
Dario Müller <moin@softwerkstatt.de>
2024-06-30 21:29:30 +0200

    fixed redirects

--- copyFiles.js
+++ copyFiles.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 var files = ['_redirects', 'robots.txt'];
-var output = 'dist';
+var output = 'dist/rupert-rosenkranz/browser';
 
 files.forEach((file) => {
     fs.copyFile(file, output + '/' + file, (err) => {
